042a893c9dcdd96089b15f6f360361493c269fd5
bielu000 <bielu000@o2.pl>
2020-01-15 07:13:10 +0100

    Up

--- src/app/main.c
+++ src/app/main.c
@@ -1,9 +1,16 @@
 #include <libopencm3/stm32/rcc.h>
-
+#include <libopencm3/stm32/gpio.h>
 
 int main()
 {
-  rcc_clock_setup_in_hse_8mhz_out_72mhz();
+  // rcc_clock_setup_in_hse_8mhz_out_72mhz();
+  // rcc_periph_clock_enable(RCC_GPIOA);
+  
+  // gpio_set_mode(GPIOA, GPIO_MODE_OUTPUT_50_MHZ, GPIO_CNF_OUTPUT_PUSHPULL, GPIO11);
 
-  return 1;
+  // gpio_toggle(GPIOA, GPIO11);
+  while(1)
+  {
+
+  }
 }
--- src/port/stm32smart/port.cmake
+++ src/port/stm32smart/port.cmake
@@ -1,9 +1,14 @@
 set(LD_SCRIPT /home/pp/STM32/STM32_Template/src/port/stm32smart/ld/STM32F103C8Tx_FLASH.ld)
 
-SET(RCC_SRC 
+set(RCC_SRC 
   /home/pp/STM32/STM32_Template/lib/libopencm3/lib/stm32/common/rcc_common_all.c
   /home/pp/STM32/STM32_Template/lib/libopencm3/lib/stm32/common/flash_common_all.c
   /home/pp/STM32/STM32_Template/lib/libopencm3/lib/stm32/f1/rcc.c
+)
+
+set(GPIO_SRC
+  /home/pp/STM32/STM32_Template/lib/libopencm3/lib/stm32/f1/gpio.c
+  /home/pp/STM32/STM32_Template/lib/libopencm3/lib/stm32/common/gpio_common_all.c
 )
 
 add_compile_definitions(STM32F1)
