0772255a118da248fefc01db9cb4ac995fd672f9
bielu000 <bielu000@o2.pl>
2020-09-04 15:31:57 +0200

    Moved dma configuration to ESP8266 driver.

--- src/drivers/include/Esp8266Wlan.hpp
+++ src/drivers/include/Esp8266Wlan.hpp
@@ -6,17 +6,28 @@
 namespace drivers
 {
   enum class Esp8266WlanMode { Client, AccessPoint, Both };
+  struct Esp8266WlanHwCfg
+  {
+    uint32_t dma_id;
+    uint32_t dma_rx_channel_id;
+    uint32_t dma_rx_nvic_irq;
+    uint32_t dma_tx_channel_id;
+    uint32_t dma_tx_nvic_channel_id;
+    uint32_t usart_id;
+    uint32_t usart_dr_addr;
+    uint32_t usart_nvic_irq;
+  };
+
   class Esp8266Wlan
   {
   public:
-    Esp8266Wlan(uint32_t usart);
+    Esp8266Wlan(const Esp8266WlanHwCfg& hw_cfg);
     //Driver
     void init();
     void connect_ap(std::string_view ssid, std::string_view password);
     void set_mode(Esp8266WlanMode mode);
 
-    //Api
     private:  
-      uint32_t usart_;
+      const Esp8266WlanHwCfg& hw_cfg_;
   };
 }
--- src/drivers/src/Esp8266Wlan.cpp
+++ src/drivers/src/Esp8266Wlan.cpp
@@ -8,65 +8,50 @@ extern "C" {
   #include <libopencm3/stm32/usart.h>
 }
 
+using namespace drivers;
+
 namespace {
-  struct DmaGeneralCfg {
-    uint32_t dma;
-    uint32_t dma_channel;
-    uint32_t periph_addr;
-    uint32_t usart_id;
-    uint32_t nvic_irq;
-  };
 
-  static constexpr size_t READ_BUFFER_SIZE = 64;
+  static constexpr size_t BUFFER_SIZE = 64;
+  static uint8_t receive_buff[BUFFER_SIZE];
 
-  [[maybe_unused]]
-  static uint8_t read_buffer[READ_BUFFER_SIZE];
+void dma_receive(const Esp8266WlanHwCfg& hw_cfg, uint8_t* buff, size_t buff_size)
+{
+	dma_channel_reset(hw_cfg.dma_id, hw_cfg.dma_rx_channel_id);
 
-  const static DmaGeneralCfg dma_cfg {
-    DMA1,
-    DMA_CHANNEL5, 
-    reinterpret_cast<uint32_t>(&USART1_DR),
-    USART1,
-    NVIC_DMA1_CHANNEL5_IRQ
-  };
+	dma_set_peripheral_address(hw_cfg.usart_dr_addr, hw_cfg.dma_rx_channel_id, hw_cfg.usart_dr_addr);
+	dma_set_memory_address(hw_cfg.dma_id, hw_cfg.dma_rx_channel_id, reinterpret_cast<uint32_t>(buff));
+	dma_set_number_of_data(hw_cfg.dma_id, hw_cfg.dma_rx_channel_id, buff_size);
+	dma_set_read_from_peripheral(hw_cfg.dma_id, hw_cfg.dma_rx_channel_id);
+	dma_enable_memory_increment_mode(hw_cfg.dma_id, hw_cfg.dma_rx_channel_id);
+  dma_enable_circular_mode(hw_cfg.dma_id, hw_cfg.dma_rx_channel_id);
+	dma_set_peripheral_size(hw_cfg.dma_id, hw_cfg.dma_rx_channel_id, DMA_CCR_PSIZE_8BIT);
+	dma_set_memory_size(hw_cfg.dma_id, hw_cfg.dma_rx_channel_id, DMA_CCR_MSIZE_8BIT);
+	dma_set_priority(hw_cfg.dma_id, hw_cfg.dma_rx_channel_id, DMA_CCR_PL_HIGH);
 
+	dma_enable_transfer_complete_interrupt(hw_cfg.dma_id, hw_cfg.dma_rx_channel_id);
+  dma_enable_half_transfer_interrupt(hw_cfg.dma_id, hw_cfg.dma_rx_channel_id);
+
+	dma_enable_channel(hw_cfg.dma_id, hw_cfg.dma_rx_channel_id);
+
+  nvic_enable_irq(hw_cfg.dma_rx_nvic_irq);  
+
+  usart_enable_rx_dma(hw_cfg.usart_id);
 }
 
- char data[64];
- size_t size = sizeof(data);
-
-void dma2_req()
-{
-	dma_channel_reset(DMA1, DMA_CHANNEL5);
-
-	dma_set_peripheral_address(DMA1, DMA_CHANNEL5, (uint32_t)&USART1_DR);
-	dma_set_memory_address(DMA1, DMA_CHANNEL5, (uint32_t)data);
-	dma_set_number_of_data(DMA1, DMA_CHANNEL5, size);
-	dma_set_read_from_peripheral(DMA1, DMA_CHANNEL5);
-	dma_enable_memory_increment_mode(DMA1, DMA_CHANNEL5);
-	dma_set_peripheral_size(DMA1, DMA_CHANNEL5, DMA_CCR_PSIZE_8BIT);
-	dma_set_memory_size(DMA1, DMA_CHANNEL5, DMA_CCR_MSIZE_8BIT);
-	dma_set_priority(DMA1, DMA_CHANNEL5, DMA_CCR_PL_HIGH);
-
-	dma_enable_transfer_complete_interrupt(DMA1, DMA_CHANNEL5);
-
-	dma_enable_channel(DMA1, DMA_CHANNEL5);
-
-  usart_enable_rx_dma(USART1);
 }
 
 namespace drivers {
 
-Esp8266Wlan::Esp8266Wlan(uint32_t usart)
-  : usart_{usart} {}
+Esp8266Wlan::Esp8266Wlan(const Esp8266WlanHwCfg& hw_cfg)
+  : hw_cfg_{hw_cfg} {}
 
 void Esp8266Wlan::init()
 {
-  /// USART && DMA1
+  /// USART && hw_cfg_.dma_id
   // -> RCC
   rcc_periph_clock_enable(RCC_USART1);
   rcc_periph_clock_enable(RCC_DMA1);
-  
 
   // -> GPIO
   gpio_set_mode(GPIO_BANK_USART1_TX, GPIO_MODE_OUTPUT_50_MHZ, 
@@ -76,22 +61,22 @@ void Esp8266Wlan::init()
     GPIO_CNF_OUTPUT_ALTFN_OPENDRAIN, GPIO_USART1_RX);
 
   // -> Configuration
-  usart_set_mode(USART1, USART_MODE_TX_RX);
-  usart_set_baudrate(USART1, 9600);
-  usart_set_parity(USART1, USART_PARITY_NONE);
-  usart_set_databits(USART1, 8);
-  usart_set_stopbits(USART1, 1);
-  usart_set_flow_control(USART1, USART_FLOWCONTROL_NONE);
-  // usart_enable_rx_interrupt(USART1);
+  usart_set_mode(hw_cfg_.usart_id, USART_MODE_TX_RX);
+  usart_set_baudrate(hw_cfg_.usart_id, 9600);
+  usart_set_parity(hw_cfg_.usart_id, USART_PARITY_NONE);
+  usart_set_databits(hw_cfg_.usart_id, 8);
+  usart_set_stopbits(hw_cfg_.usart_id, 1);
+  usart_set_flow_control(hw_cfg_.usart_id, USART_FLOWCONTROL_NONE);
+  usart_enable_rx_interrupt(hw_cfg_.usart_id);
 
-  // -> ISR
-  // nvic_enable_irq(NVIC_USART1_IRQ);
-  
+  nvic_enable_irq(hw_cfg_.usart_nvic_irq);
+
   // -> Enable 
-  usart_enable(USART1);
+  usart_enable(hw_cfg_.usart_id);
 
-	nvic_set_priority(NVIC_DMA1_CHANNEL5_IRQ, 0);
-	nvic_enable_irq(NVIC_DMA1_CHANNEL5_IRQ);
+  //Enable DMA_RX IRQ
+  nvic_set_priority(hw_cfg_.dma_rx_nvic_irq, 0);
+	nvic_enable_irq(hw_cfg_.dma_rx_nvic_irq);
 
 }
 
@@ -100,8 +85,7 @@ void Esp8266Wlan::connect_ap(std::string_view ssid, std::string_view password)
   [[maybe_unused]] auto x = ssid;
   [[maybe_unused]] auto y = password;
 
-  // dma_read(dma_cfg, read_buffer, sizeof(read_buffer));
-  dma2_req();
+  dma_receive(hw_cfg_, receive_buff, sizeof(receive_buff));
 
 }
 
@@ -110,48 +94,32 @@ void Esp8266Wlan::set_mode(Esp8266WlanMode mode)
   [[maybe_unused]] auto x = mode;
 }  
 
-//ISR
-// void usart1_isr()
-// {
-  // if (usart_get_flag(USART1, USART_FLAG_TXE) != 0) 
-  // {
-  //   sendData();
-  // }
+}
 
-  // if (usart_get_flag(USART1, USART_FLAG_RXNE) != 0) // when data is ready to read
-  // {
-  //   readData(); 
-  // }
-// } 
-} //namespace drivers
-
-// void usart1_isr()
-// {
-//   if (usart_get_flag(USART1, USART_FLAG_TXE) != 0) 
-//   {
-//     [[maybe_unused]] int x = 10;
-//   }
-
-//   if (usart_get_flag(USART1, USART_FLAG_RXNE) != 0) // when data is ready to read
-//   {
-//     [[maybe_unused]]
-//     auto data = usart_recv(USART1);
-//     [[maybe_unused]] int x = 10;
-//   }
-// } 
+void usart1_isr()
+{
+  if (usart_get_flag(USART1, USART_FLAG_IDLE))
+  {
+  }
+} 
 
 void dma1_channel5_isr(void)
 {
-  if (dma_get_interrupt_flag(DMA1, DMA_CHANNEL5, DMA_HTIF))
+  if (dma_get_interrupt_flag(USART1, DMA_CHANNEL5, DMA_TCIF))
   {
     [[maybe_unused]] int x = 10;
+    dma_clear_interrupt_flags(USART1, DMA_CHANNEL5, DMA_TCIF);
   }
-  else if (dma_get_interrupt_flag(DMA1, DMA_CHANNEL5, DMA_TCIF))
+
+  else if (dma_get_interrupt_flag(USART1, DMA_CHANNEL5, DMA_HTIF))
   {
     [[maybe_unused]] int x = 10;
+    dma_clear_interrupt_flags(USART1, DMA_CHANNEL5, DMA_HTIF);
   }
-  else if (dma_get_interrupt_flag(DMA1, DMA_CHANNEL5, DMA_TEIF))
+
+  else if (dma_get_interrupt_flag(USART1, DMA_CHANNEL5, DMA_TEIF))
   {
     [[maybe_unused]] int x = 10;
+        dma_clear_interrupt_flags(USART1, DMA_CHANNEL5, DMA_TEIF);
   }
 }
