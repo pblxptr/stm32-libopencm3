ae9fafe60914577ac395e5b789f7bcbf05c3ceae
bielu000 <bielu000@o2.pl>
2021-03-22 14:17:35 +0100

    Travis config update.

--- firmware/tools/build-env.sh
+++ firmware/tools/build-env.sh
@@ -7,6 +7,9 @@ export TEST_DIR="$REPO_ROOT/test"
 export EXTERNAL_LIB_DIR="$REPO_ROOT/lib"
 export TOOLS_DIR="$REPO_ROOT/tools"
 
+### PREPARE BUILD DIR ###
+mkdir -p $BUILD_DIR
+
 ### SOLUTIONS ###
 export APP_SOLUTION_CMAKE_DIR_PATH="$REPO_ROOT"
 export UNITTESTS_SOLUTION_CMAKE_DIR_PATH="$TEST_DIR/unittests"
@@ -18,3 +21,5 @@ alias clean="$REPO_ROOT/tools/clean.py"
 ### BUILD ALIASES ###
 alias buildapp="cd "$BUILD_DIR" && build --solution=app --board=black_pill --toolchain=arm --trace_level=TRACE_LVL_1"
 alias run_ts="cd "$BUILD_DIR" && build --solution=unittests --board=linux --toolchain=x86_64 --trace_level=TRACE_LVL_3 && ./bin/Henhouse-UnitTests"
+
+shopt -s expand_aliases
